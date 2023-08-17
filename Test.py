6f041102fb05233dbc6fc6c4215bedbc8f9822a8
navid <navid.tavakoli1988@gmail.com>
2023-08-18 00:03:48 +0200

    test commit

---
+++ Test.py
@@ -0,0 +1,178 @@
+import tkinter as tk
+from tkinter import ttk
+import requests
+from PIL import Image, ImageTk
+import csv
+import tkinter.messagebox as messagebox
+
+class MovieSearchApp:
+    def __init__(self, root):
+        self.root = root
+        self.api_key = "45edf973"
+        self.initialize_ui()
+
+    def initialize_ui(self):
+        self.root.title("Movie Search App")
+
+        input_frame = tk.Frame(self.root)
+        input_frame.pack(padx=10, pady=10)
+
+        movie_name_label = tk.Label(input_frame, text="Movie Name:")
+        movie_name_label.grid(row=0, column=0, sticky="e")
+        self.movie_name_entry = tk.Entry(input_frame)
+        self.movie_name_entry.grid(row=0, column=1, padx=5)
+
+        movie_year_label = tk.Label(input_frame, text="Year of Release:")
+        movie_year_label.grid(row=0, column=2, sticky="e")
+        self.movie_year_entry = tk.Entry(input_frame)
+        self.movie_year_entry.grid(row=0, column=3, padx=5)
+
+        movie_type_label = tk.Label(input_frame, text="Type:")
+        movie_type_label.grid(row=0, column=4, sticky="e")
+        self.movie_type_combo = ttk.Combobox(input_frame, values=["movie", "series", "episode"])
+        self.movie_type_combo.grid(row=0, column=5, padx=5)
+
+        search_button = tk.Button(input_frame, text="Search", command=self.search_movie)
+        search_button.grid(row=0, column=6, padx=10)
+
+        separator = ttk.Separator(self.root, orient="horizontal")
+        separator.pack(fill="x", padx=10, pady=5)
+
+        self.result_frame = tk.Frame(self.root)
+        self.result_frame.pack(padx=10, pady=10)
+
+        # Create a Label for the poster image
+        self.poster_label = tk.Label(self.result_frame)
+        self.poster_label.grid(row=0, column=0, rowspan=16, padx=10, pady=5)
+
+        # Create a Treeview for displaying details
+        self.tree = ttk.Treeview(self.result_frame, columns=("detail", "value"))
+        self.tree.heading("#1", text="Detail")
+        self.tree.heading("#2", text="Value")
+        self.tree.column("#1", width=150)
+        self.tree.column("#2", width=300)
+        self.tree.grid(row=0, column=1, padx=10, pady=5)
+
+        self.initialize_checkboxes_and_button()
+
+    def search_movie(self):
+        movie_name = self.movie_name_entry.get()
+        movie_year = self.movie_year_entry.get()
+        movie_type = self.movie_type_combo.get()
+
+        url = f"http://www.omdbapi.com/?apikey={self.api_key}&t={movie_name}&y={movie_year}&type={movie_type}"
+        response = requests.get(url)
+        movie_data = response.json()
+
+        self.update_ui(movie_data)
+
+    def update_ui(self, movie_data):
+        self.tree.delete(*self.tree.get_children())  # Clear existing table rows
+
+        if movie_data.get("Response") == "True":
+            poster_url = movie_data.get("Poster")
+            if poster_url != "N/A":
+                image = Image.open(requests.get(poster_url, stream=True).raw)
+                image.thumbnail((200, 300))
+                self.poster_image = ImageTk.PhotoImage(image)
+
+                # Set the poster image to the label
+                self.poster_label.configure(image=self.poster_image)
+
+            detail_labels = [
+                "Title:", "Genre:", "Runtime:", "Year:", "Director:",
+                "IMDB Rating:", "IMDB Votes:", "Rotten Tomatoes Rating:",
+                "Actors:", "IMDB ID:", "Type:", "Rated:", "Released:",
+                "Writer:", "Country:", "Awards:"
+            ]
+            self.detail_labels = detail_labels
+            detail_values = [
+                movie_data.get("Title"), movie_data.get("Genre"), movie_data.get("Runtime"),
+                movie_data.get("Year"), movie_data.get("Director"), movie_data.get("imdbRating"),
+                movie_data.get("imdbVotes"), self.get_rotten_tomatoes_rating(movie_data),
+                movie_data.get("Actors"), movie_data.get("imdbID"), movie_data.get("Type"),
+                movie_data.get("Rated"), movie_data.get("Released"), movie_data.get("Writer"),
+                movie_data.get("Country"), movie_data.get("Awards")
+            ]
+            self.detail_values = detail_values
+
+            for label_text, value_text in zip(detail_labels, detail_values):
+                self.tree.insert("", "end", values=(label_text, value_text))
+
+            self.initialize_checkboxes_and_button()
+
+    def get_rotten_tomatoes_rating(self, movie_data):
+        ratings = movie_data.get("Ratings")
+        if ratings:
+            for rating in ratings:
+                if "Rotten Tomatoes" in rating['Source']:
+                    return rating['Value']
+        return "N/A"
+
+    def initialize_checkboxes_and_button(self):
+        self.watched_var = tk.IntVar(value=0)
+        self.want_to_watch_var = tk.IntVar(value=0)
+
+        # Add these lines in the initialize_checkboxes_and_button method
+        watched_radio = tk.Radiobutton(self.result_frame, text="Watched", variable=self.watched_var, value=1)
+        watched_radio.grid(row=16, column=1, padx=10, pady=5)
+
+        want_to_watch_radio = tk.Radiobutton(self.result_frame, text="I Want to Watch", variable=self.watched_var,
+                                             value=0)
+        want_to_watch_radio.grid(row=16, column=2, padx=10, pady=5)
+
+
+
+        save_button = tk.Button(self.result_frame, text="Save", command=self.save_result)
+        save_button.grid(row=17, column=1, columnspan=2, padx=10, pady=5)
+
+    def save_result(self):
+        watched = 1 if self.watched_var.get() == "NO" else "YES"
+        want_to_watch = 1 if self.want_to_watch_var.get() == "YES" else "NO"
+
+        title = self.detail_values[0]  # Get the title from detail_values
+
+        # Check if the title already exists in the CSV file
+        is_title_exists = False
+        try:
+            with open("movie_results.csv", 'r', newline='') as csv_file:
+                csv_reader = csv.reader(csv_file)
+                for row in csv_reader:
+                    if row and row[0] == title:
+                        is_title_exists = True
+                        break
+        except FileNotFoundError:
+            pass
+
+        if not is_title_exists:
+            # Write header row if the file is empty
+            is_file_empty = False
+            try:
+                with open("movie_results.csv", 'r') as csv_file:
+                    is_file_empty = csv_file.read().strip() == ''
+            except FileNotFoundError:
+                is_file_empty = True
+
+            if is_file_empty:
+                with open("movie_results.csv", 'a', newline='') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow(self.detail_labels + ["Watched", "I Want to Watch"])  # Add new columns
+
+            # Append the data to the CSV file
+            with open("movie_results.csv", 'a', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow(self.detail_values + [watched, want_to_watch])  # Append data
+
+            # Show success message box
+            messagebox.showinfo("Success", "Data has been saved")
+
+        else:
+            # Show warning message box
+            messagebox.showwarning("Warning", f"Title '{title}' already exists")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MovieSearchApp(root)
+    root.mainloop()
